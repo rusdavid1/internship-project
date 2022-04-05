ca84313102927c68fc88df2550597daf33cf6687
rusdavid1 <rusdavid99@gmail.com>
2022-04-05 14:59:04 +0300

    Subscribed to delete event

--- src/EventSubscriber/SoftDeleteSubscriber.php
+++ src/EventSubscriber/SoftDeleteSubscriber.php
@@ -4,13 +4,28 @@ declare(strict_types=1);
 
 namespace App\EventSubscriber;
 
+use Gedmo\SoftDeleteable\SoftDeleteableListener;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Contracts\EventDispatcher\Event;
 
-class SoftDeleteSubscriber
+class SoftDeleteSubscriber implements EventSubscriberInterface
 {
-    public function test(Event $event)
+    public static function getSubscribedEvents(): array
+    {
+        return[
+          SoftDeleteableListener::PRE_SOFT_DELETE => 'checkForTrainer'
+        ];
+    }
+
+    public function checkForTrainer(Event $event)
     {
         var_dump($event);
+//        get user
+
+//        check role
+
+//        if trainer check programmes he's assigned to
+
+//        all programmes he's assigned to will get null trainer
     }
 }
