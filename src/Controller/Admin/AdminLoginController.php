95be92f68e46ded0245c0702f69e1ce47e357157
rusdavid1 <rusdavid99@gmail.com>
2022-04-06 17:35:40 +0300

    Added errors

--- src/Controller/Admin/AdminLoginController.php
+++ src/Controller/Admin/AdminLoginController.php
@@ -7,17 +7,21 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminLoginController extends AbstractController
 {
     /**
      * @Route (path="/admin/login", name="admin_login")
      */
-    public function loginAction(): Response
+    public function loginAction(AuthenticationUtils $authenticationUtils): Response
     {
-//        $error = $authenticationUtils->getLastAuthenticationError();
-//        $lastUsername = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('admin/adminLogin.html.twig', []);
+        return $this->render('admin/adminLogin.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 }
