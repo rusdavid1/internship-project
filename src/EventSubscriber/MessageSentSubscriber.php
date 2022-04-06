f25050e9dcd73f5041986fac85e7d8c0c821cc20
rusdavid1 <rusdavid99@gmail.com>
2022-04-06 09:53:56 +0300

    Sending sms to all users, refactored repeating method

--- src/EventSubscriber/MessageSentSubscriber.php
+++ src/EventSubscriber/MessageSentSubscriber.php
@@ -12,6 +12,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Contracts\EventDispatcher\Event;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MessageSentSubscriber implements EventSubscriberInterface
 {
@@ -19,16 +21,20 @@ class MessageSentSubscriber implements EventSubscriberInterface
 
     private MessageBusInterface $messageBus;
 
+    private HttpClientInterface $httpClient;
+
     private Mailer $mailerWrapper;
 
     public function __construct(
         MessageBusInterface $messageBus,
         Mailer $mailerWrapper,
-        UserRepository $userRepository
+        UserRepository $userRepository,
+        HttpClientInterface $smsClient
     ) {
         $this->messageBus = $messageBus;
         $this->mailerWrapper = $mailerWrapper;
         $this->userRepository = $userRepository;
+        $this->httpClient = $smsClient;
     }
 
     public static function getSubscribedEvents(): array
@@ -42,8 +48,20 @@ class MessageSentSubscriber implements EventSubscriberInterface
     {
         $users = $this->userRepository->findAll();
 
-        $this->messageBus->dispatch(new Envelope(new SmsNotification('Well, hello there')));
+        $message = 'Well, hello there';
+
         foreach ($users as $user) {
+            $this->messageBus->dispatch(new Envelope(new SmsNotification($message)));
+            try {
+                $response = $this->httpClient->request(
+                    'POST',
+                    'messages',
+                    ['json' => ['receiver' => $user->phoneNumber,'body' => $message]]
+                );
+                $test = $response->getStatusCode();
+            } catch (TransportExceptionInterface $e) {
+                echo $e->getMessage();
+            }
             $this->mailerWrapper->sendAnnouncementEmail($user);
         }
     }
--- src/MessageHandler/SmsNotificationHandler.php
+++ src/MessageHandler/SmsNotificationHandler.php
@@ -5,38 +5,16 @@ declare(strict_types=1);
 namespace App\MessageHandler;
 
 use App\Message\SmsNotification;
-use App\Repository\UserRepository;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class SmsNotificationHandler implements MessageHandlerInterface, LoggerAwareInterface
 {
     use LoggerAwareTrait;
 
-    private UserRepository $userRepository;
-
-    private HttpClientInterface $httpClient;
-
-    public function __construct(
-        HttpClientInterface $smsClient,
-        UserRepository $userRepository
-    ) {
-        $this->httpClient = $smsClient;
-        $this->userRepository = $userRepository;
-    }
-
     public function __invoke(SmsNotification $message): void
     {
-        $users = $this->userRepository->findAll();
-
-        foreach ($users as $user) {
-            $this->httpClient->request('POST', 'messages', [
-                'json' => ['receiver' => $user->phoneNumber,'body' => $message->getContent()]
-            ]);
-
-            $this->logger->info('SMS sent', ['receivedBy' => $user->phoneNumber]);
-        }
+        $this->logger->info('Sms sent');
     }
 }
