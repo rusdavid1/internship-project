ee3159dabb3d8161c39785cd312686809c79bc89
rusdavid1 <65897158+rusdavid1@users.noreply.github.com>
2022-04-08 10:15:25 +0300

    Implemented admin login and dashboard (#47)
    * Added login route, styled the dashboard, footer and header
    
    * Styled button and form
    
    * Fixed code sniffer eroors
    
    * Renamed providers
    
    * Added public access
    
    * Created a admin namespace
    
    * Removed unused lines
    
    * Added title
    
    * Added logout
    
    * Implemented logout
    
    * Added errors
    
    * Reverted package  version
    
    * Solved cs errors
    
    * Refactored code, added logout btn
    
    * Corrected typo
    
    * Added default route in annotations.yaml

---
+++ migrations/Version20220406144606.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220406144606 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD phone_number VARCHAR(255) NOT NULL, DROP deletedAt');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6496B01BC5B ON user (phone_number)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE messenger_messages CHANGE body body LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE headers headers LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE queue_name queue_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE programme CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE description description LONGTEXT NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('ALTER TABLE room CHANGE name name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+        $this->addSql('DROP INDEX UNIQ_8D93D6496B01BC5B ON user');
+        $this->addSql('ALTER TABLE user ADD deletedAt DATETIME DEFAULT NULL, DROP phone_number, CHANGE first_name first_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE last_name last_name VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE cnp cnp CHAR(13) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE email email VARCHAR(180) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`, CHANGE plain_password plain_password VARCHAR(255) NOT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractController
+{
+    /**
+     * @Route (methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $logoutUrl = $this->generateUrl('admin_logout');
+
+        return $this->render('admin/adminDashboard.html.twig', [
+            'logoutUrl' => $logoutUrl,
+        ]);
+    }
+}
