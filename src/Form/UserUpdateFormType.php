79fbb911558ce1f9140535fd941b95c8313b9884
rusdavid1 <rusdavid99@gmail.com>
2022-04-11 16:44:48 +0300

    Fixed footer

--- src/Form/UserUpdateFormType.php
+++ src/Form/UserUpdateFormType.php
@@ -15,11 +15,24 @@ class UserUpdateFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName', TextType::class, ['attr' => ['class' => 'bg-red-600']])
+            ->add('firstName', TextType::class)
             ->add('lastName', TextType::class)
             ->add('email', EmailType::class)
             ->add('phoneNumber', TextType::class)
-            ->add('submit', SubmitType::class)
+            ->add(
+                'submit',
+                SubmitType::class,
+                ['attr' =>
+                    ['class' =>
+                        'rounded-full 
+                        px-4 
+                        py-3 
+                        bg-gradient-to-r 
+                        from-indigo-900
+                         to-purple-900 
+                         text-white
+                          text-xl']]
+            )
         ;
     }
 }
