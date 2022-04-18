9565d3ca823c8b33641d79a7fed623161375e506
rusdavid1 <rusdavid99@gmail.com>
2022-04-18 10:41:08 +0300

    Added secure route

---
+++ src/Controller/Api/JoinProgrammeController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class JoinProgrammeController
+{
+    /**
+     * @Route (path="/api/join", methods={"POST"})
+     */
+    public function index()
+    {
+
+    }
+}
