95d415d0570888a84f1e81e4bb0ce2b989c92eca
rusdavid1 <rusdavid99@gmail.com>
2022-04-19 17:05:03 +0300

    Fixed validator and worng import

--- src/Validator/CnpValidator.php
+++ src/Validator/CnpValidator.php
@@ -15,6 +15,12 @@ class CnpValidator extends ConstraintValidator
             throw new UnexpectedTypeException($constraint, Cnp::class);
         }
 
+        if (null === $value) {
+            $this->context->buildViolation($constraint->message)->atPath('')->addViolation();
+
+            return;
+        }
+
         $regEx1 = '/^([1-8]\d{2})(0[1-9]|1[0-2])(0[1-9]|1\d|2\d|3[0-1])';
         $regEx2 = '(0[1-9]|1\d|2\d|3\d|4[0-5]|5[1-2])(\d|[1-9]\d|[1-9]\d{3})$/m';
         $regEx = $regEx1 . $regEx2;
